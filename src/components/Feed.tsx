d1434c7a3f0e3e667ed00771c2ec17d3bd3bf3ce
masa <iwamasa.0526.to@gmail.com>
2021-01-02 20:36:51 +0900

    tweetinputエリア完了

--- src/components/Feed.tsx
+++ src/components/Feed.tsx
@@ -1,11 +1,14 @@
 import React from 'react'
+import styles from "./Feed.module.css"
 import { auth } from "../firebase"
+import { TweetInput } from './TweetInput'
 
 export const Feed = () => {
     return (
-        <div>
-            <button onClick={() => auth.signOut()}>Logout</button>
+        <div className={styles.feed}>
             feed
+            <TweetInput />
+            <button onClick={() => auth.signOut()}>Logout</button>
         </div>
     )
 }
